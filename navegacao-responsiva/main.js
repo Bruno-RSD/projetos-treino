84bb364777a45fdf9e00afcdaf582b122c5d3724
Bruno-RSD <46577782+Bruno-RSD@users.noreply.github.com>
2024-03-01 15:41:54 -0300

    EndResponsiveNav
    Ao meu ver, concluído o projeto da navegação responsiva. Com certeza há outras maneiras de ser feitas. E com certeza maneiras mais efetivas.
    
    Ao fim do projeto não achei que foi a programação mais limpa possível. Esse é um dos pontos a melhorar na programação desse projeto, principalmente na seleção e alteração das classes, ao meu ver é claro.
    
    ~Bruno_RSD

--- navegacao-responsiva/main.js
+++ navegacao-responsiva/main.js
@@ -1,7 +1,25 @@
 const buttonHamb = document.querySelector(".hamburguer");
+let segundaNaveg = document.querySelector(".segundaNavegacao");
+let hambContainer = document.querySelector(".container-hamburguer");
 
-buttonHamb.addEventListener("click", (e) => {
-  e.target.classList.toggle("btnPressionado");
+buttonHamb.addEventListener("click", function () {
+  buttonHamb.classList.toggle("btnPressionado");
+  telaNavegacao2();
+  // console.log("Fui clicado");
+});
 
-  //   console.log("Fui clicado");
+function telaNavegacao2() {
+  if (buttonHamb.classList.contains("btnPressionado")) {
+    segundaNaveg.style.display = "flex";
+  } else {
+    segundaNaveg.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth >= 1075) {
+    buttonHamb.classList.toggle("btnPressionado", false);
+    telaNavegacao2();
+    // console.log("Teste janela");
+  }
 });
